62824eab9369abb99e94ba9f0150557c41dc3f83
Daniel Petrie <dan.petrie@cartalyst.com>
2012-08-18 09:19:40 -0700

    adding config back in, lets see if this clears history

---
+++ controllers/sentrysocial.php
@@ -0,0 +1,33 @@
+<?php
+
+return array(
+
+	/**
+	 * Callback URL
+	 */
+	'callback_url' => 'sentrysocial/auth/callback',
+
+	/**
+	 * Social Providers
+	 */
+	'providers' => array(
+
+		/**
+		 * Examples
+		 *
+		 *	 'twitter' => array(
+		 *		'app_id'     => 'your app id',
+		 *		'app_secret' => 'your app secrete',
+		 *		'driver'     => 'OAuth',
+		 *	),
+		 *
+		 * 'facebook' => array(
+		 *		'app_id'     => 'your app id',
+		 *		'app_secret' => 'your app secret',
+		 *      'scope'      => array('offline_access'),
+		 *		'driver'     => 'OAuth2'
+		 *	),
+		 */
+
+	)
+);
