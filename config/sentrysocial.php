4ffec62dd3fa5b34c5edb71c8a4c92b7af6f5526
Daniel Petrie <dan.petrie@cartalyst.com>
2012-08-17 22:00:10 -0700

    Update config/sentrysocial.php

--- config/sentrysocial.php
+++ config/sentrysocial.php
@@ -16,15 +16,15 @@ return array(
 		 * Examples
 		 *
 		 *	 'twitter' => array(
-		 *		'app_id'     => 'gHvPp5IlA4fKxzTnS8a0w',
-		 *		'app_secret' => 'KajMPNHhiAoI5sn1Wvh33NLJF0L0bnX9N3l89ZWTXU',
+		 *		'app_id'     => 'your app id',
+		 *		'app_secret' => 'your app secrete',
 		 *		'driver'     => 'OAuth',
 		 *	),
 		 *
 		 * 'facebook' => array(
 		 *		'app_id'     => 'your app id',
 		 *		'app_secret' => 'your app secret',
-		 *      'scope'      => array('offline_access'),
+		 *              'scope'      => array('offline_access'),
 		 *		'driver'     => 'OAuth2' // or OAuth - what protocal it uses
 		 *	),
 		 */
