99de1c5ffc04981f98221e7b4d050547d27b6fb6
Daniel Petrie <dan.petrie@cartalyst.com>
2012-08-17 21:49:17 -0700

    Update config/sentrysocial.php

--- config/sentrysocial.php
+++ config/sentrysocial.php
@@ -29,24 +29,5 @@ return array(
 		 *	),
 		 */
 
-		'twitter' => array(
-			'app_id'     => 'gHvPp5IlA4fKxzTnS8a0w',
-			'app_secret' => 'KajMPNHhiAoI5sn1Wvh33NLJF0L0bnX9N3l89ZWTXU',
-			'driver'     => 'OAuth',
-		),
-
-		'facebook' => array(
-			'app_id'     => '507183095963402',
-			'app_secret' => '466168e23984ae645a898790c2f342d3',
-			'scope'      => array('email', 'offline_access'),
-			'driver'     => 'OAuth2'
-		),
-
-		'google' => array(
-			'app_id'     => '496043082049',
-			'app_secret' => '7SBROVhOec1_d1Gqk4UYZH_l',
-			'driver'     => 'OAuth2'
-		)
-
 	)
 );
