32a095b9382d1d05ace15b367ee968f02c6f7278
drsii <dan.syme@gmail.com>
2012-11-02 10:43:12 -0700

    licence udpates
    Signed-off-by: drsii <dan.syme@gmail.com>

--- sentrysocial.php
+++ sentrysocial.php
@@ -4,10 +4,12 @@
  *
  * NOTICE OF LICENSE
  *
+ * Licensed under the Cartalyst Software Licence.
+ *
  * @package    Sentry Social
  * @version    1.1
  * @author     Cartalyst LLC
- * @license    http://getplatform.com/manuals/sentrysocial/license
+ * @license    Cartalyst Software Licence - http://cartalyst.com/licence
  * @copyright  (c) 2011 - 2012, Cartalyst LLC
  * @link       http://cartalyst.com
  */
