fb769d62bfa71b994691065fd625687eb49f74be
Ben Corlett <bencorlett@me.com>
2013-09-12 14:55:53 +1000

    Tidy up and docblock the Manager class.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- src/Cartalyst/SentrySocial/Manager.php
+++ src/Cartalyst/SentrySocial/Manager.php
@@ -26,54 +26,112 @@ use Cartalyst\Sentry\Sessions\NativeSession;
 use Cartalyst\Sentry\Sessions\SessionInterface;
 use Cartalyst\Sentry\Sentry;
 use Closure;
+use Illuminate\Events\Dispatcher;
 
 class Manager {
 
+	/**
+	 * The shared Sentry instance.
+	 *
+	 * @var \Cartalyst\Sentry\Sentyr
+	 */
 	protected $sentry;
 
+	/**
+	 * The link provider, used for associating users
+	 * with OAuth providers.
+	 *
+	 * @var \Cartalyst\SentrySocial\Links\ProviderInterface
+	 */
+	protected $linkProvider;
+
+	/**
+	 * The request provider.
+	 *
+	 * @var \Cartalyst\SentrySocial\RequestProviders\ProviderInterface
+	 */
 	protected $requestProvider;
 
+	/**
+	 * A Sentry session driver.
+	 *
+	 * @var \Cartalyst\Sentry\Sessions\SessionInterface
+	 */
 	protected $session;
-
-	protected $linkProvider;
 
 	/**
 	 * The event dispatcher instance.
 	 *
-	 * @var Illuminate\Events\Dispacher
+	 * @var Illuminate\Events\Dispatcher
 	 */
 	protected $dispatcher;
 
+	/**
+	 * Array of connectiosn (credentials for
+	 * creating provider instances).
+	 *
+	 * @var array
+	 */
 	protected $connections = array();
 
+	/**
+	 * Array of initialized providers.
+	 *
+	 * @var array
+	 */
 	protected $providers = array();
 
-	public function __construct(Sentry $sentry, RequestProviderInterface $requestProvider = null, SessionInterface $session = null, Dispacher $dispatcher = null, LinkProviderInterface $linkProvider = null)
+	/**
+	 * Create a new Sentry Social Manager instance.
+	 *
+	 * @param  \Cartalyst\Sentry\Sentry  $sentry
+	 * @param  \Cartalyst\SentrySocial\Links\ProviderInterface  $linkProvider
+	 * @param  \Cartalyst\SentrySocial\RequestProviders\ProviderInterface  $requestProvider
+	 * @param  \Cartalyst\Sentry\Sessions\SessionInterface  $session
+	 * @param  \Illuminate\Events\Dispatcher  $dispatcher
+	 * @return void
+	 */
+	public function __construct(Sentry $sentry, LinkProviderInterface $linkProvider = null, RequestProviderInterface $requestProvider = null, SessionInterface $session = null, Dispatcher $dispatcher = null)
 	{
-		$this->sentry = $sentry;
+		$this->sentry          = $sentry;
+		$this->linkProvider    = $linkProvider ?: new LinkProvider;
 		$this->requestProvider = $requestProvider ?: new NativeRequestProvider;
-		$this->session = $session ?: new NativeSession('cartalyst_sentry_social');
+		$this->session         = $session ?: new NativeSession('cartalyst_sentry_social');
 
 		if (isset($dispatcher))
 		{
 			$this->dispatcher = $dispatcher;
 		}
-
-		$this->linkProvider = $linkProvider ?: new LinkProvider;
 	}
 
+	/**
+	 * Make a provider with the given connection slug
+	 * and an optional callback URI. Instances of
+	 * providers are cached.
+	 *
+	 * @param  string  $slug
+	 * @param  string  $callbackUri
+	 * @return mixed
+	 */
 	public function make($slug, $callbackUri = null)
 	{
 		if ( ! isset($this->providers[$slug]))
 		{
-			$connection = $this->getConnection($slug);
-
-			$this->providers[$slug] = $this->createProvider($connection, $callbackUri);
+			$this->providers[$slug] = $this->createProvider($slug, $callbackUri);
 		}
 
 		return $this->providers[$slug];
 	}
 
+	/**
+	 * Returns the authorize URI for a connection with the given
+	 * slug. Abstracts away the differences between OAuth1 and
+	 * OAuth2 for a uniform API.
+	 *
+	 * @param  string  $slug
+	 * @param  string  $callbackUri
+	 * @return string
+	 */
 	public function getAuthorizeUri($slug, $callbackUri = null)
 	{
 		$provider = $this->make($slug, $callbackUri);
@@ -93,14 +151,23 @@ class Manager {
 		return $provider->getAuthorizeUri();
 	}
 
+	/**
+	 * Authenticate against the current service. A closure may be provided
+	 * for a callback upon authentication as a shortcut for subscribing
+	 * to an event.
+	 *
+	 * @param  string  $slug
+	 * @param  Closure $callback
+	 * @param  bool    $remmber
+	 * @return \Cartalyst\Sentry\Users\UserInterface
+	 */
 	public function authenticate($slug, Closure $callback = null, $remember = false)
 	{
 		// If a callback was supplied, we'll treat it as
-		// a global authenticating callback. Specific
-		// callbacks for registering and existing
-		// users can be registered outside of
-		// this method.
-		if ($callback) $this->authenticating($callback);
+		// a global linking callback. Specific callbacks
+		// for registering and existing users can be
+		// registered outside of this method.
+		if ($callback) $this->linking($callback);
 
 		$provider = $this->make($slug, $callbackUri);
 		$token    = $this->retrieveToken($provider);
@@ -113,6 +180,51 @@ class Manager {
 		return $user;
 	}
 
+	/**
+	 * Register a callback for when a new user is registered
+	 * through an OAuth provider.
+	 *
+	 * @param  \Closure $callback
+	 * @return void
+	 */
+	public function registering(Closure $callback)
+	{
+		$this->registerEvent('registering', $callback);
+	}
+
+	/**
+	 * Register a callback for when an existing user is
+	 * linked with an OAuth provider.
+	 *
+	 * @param  \Closure $callback
+	 * @return void
+	 */
+	public function existing(Closure $callback)
+	{
+		$this->registerEvent('existing', $callback);
+	}
+
+	/**
+	 * Register a callback for when a user of any type is
+	 * linked with an OAuth provider.
+	 *
+	 * @param  \Closure $callback
+	 * @return void
+	 */
+	public function linking(Closure $callback)
+	{
+		$this->registerEvent('linking', $callback);
+	}
+
+	/**
+	 * Retrieves a link and associates a user (will lazily
+	 * create one) for the given slug, provider and token.
+	 *
+	 * @param  string  $slug
+	 * @param  mixed   $provider
+	 * @param  mixed   $token
+	 * @return \Cartalyst\SentrySocial\Links\LinkInterface
+	 */
 	protected function link($slug, $provider, $token)
 	{
 		$link = $this->linkProvider->findLink($slug, $provider);
@@ -172,7 +284,14 @@ class Manager {
 		return $link;
 	}
 
-	protected function login(UserInterface $user, $remember)
+	/**
+	 * Logs the given user into Sentry.
+	 *
+	 * @param  \Cartalyst\Sentry\Users\UserInterface  $user
+	 * @param  bool  $remember
+	 * @return void
+	 */
+	protected function login(UserInterface $user, $remember = false)
 	{
 		$throttleProvider = $this->sentry->getThrottleProvider();
 
@@ -191,6 +310,14 @@ class Manager {
 		$this->sentry->login($user, $remember);
 	}
 
+	/**
+	 * Retrieves a token (OAuth1 token credentials or OAuth2 access
+	 * token) for the given provider, abstracting away the
+	 * differences from the user.
+	 *
+	 * @param  mixed  $provider
+	 * @return mixed
+	 */
 	protected function retrieveToken($provider)
 	{
 		if ($this->determineOAuthType($provider) == 1)
@@ -227,11 +354,24 @@ class Manager {
 		return $accessToken;
 	}
 
+	/**
+	 * Add a connection to the manager.
+	 *
+	 * @param  string  $slug
+	 * @param  array   $connection
+	 * @return void
+	 */
 	public function addConnection($slug, array $connection)
 	{
 		$this->connections[$slug] = $connection;
 	}
 
+	/**
+	 * Add multple connections to the manager.
+	 *
+	 * @param  array  $connections
+	 * @return void
+	 */
 	public function addConnections(array $connections)
 	{
 		foreach ($connections as $slug => $connection)
@@ -240,6 +380,13 @@ class Manager {
 		}
 	}
 
+	/**
+	 * Retrieve a connection with the given slug.
+	 *
+	 * @param  string  $slug
+	 * @return array
+	 * @throws \RuntimeException
+	 */
 	public function getConnection($slug)
 	{
 		if ( ! isset($this->connections[$slug]))
@@ -251,19 +398,39 @@ class Manager {
 	}
 
 	/**
+	 * Get all connections associated with the
+	 * manager.
+	 *
+	 * @return array
+	 */
+	public function getConnections()
+	{
+		return $this->connections;
+	}
+
+	/**
 	 * Set the event dispatcher.
 	 *
-	 * @param  Illuminate\Events\Dispatcher
+	 * @param  \Illuminate\Events\Dispatcher
 	 * @return void
 	 */
-	public function setDispatcher(Dispacher $dispatcher)
+	public function setDispatcher(Dispatcher $dispatcher)
 	{
 		$this->dispatcher = $dispatcher;
 	}
 
-	protected function createProvider($connection, $callbackUri = null)
+	/**
+	 * Creates a provider from a connection with the given slug.
+	 *
+	 * @param  string  $slug
+	 * @param  string  $callbackUri
+	 * @return mixed
+	 */
+	protected function createProvider($slug, $callbackUri = null)
 	{
-		$this->validateConnection($connection);
+		$connection = $this->getConnection($slug);
+
+		$this->validateConnection($slug, $connection);
 
 		$oauthType = $this->determineOAuthType($connection['driver']);
 
@@ -275,7 +442,15 @@ class Manager {
 		return $this->createOAuth2Provider($connection, $callbackUri);
 	}
 
-	protected function validateConnection($connection)
+	/**
+	 * Validates the given connection is satisfactory to initiate a driver.
+	 *
+	 * @param  string  $slug
+	 * @param  array   $connection
+	 * @return void
+	 * @throws \InvalidArgumentException
+	 */
+	protected function validateConnection($slug, array $connection)
 	{
 		if ( ! isset($connection['driver']))
 		{
@@ -288,6 +463,15 @@ class Manager {
 		}
 	}
 
+	/**
+	 * Determines the OAuth type of the given provider. The
+	 * provider may be a driver name (string) or a provider
+	 * instance.
+	 *
+	 * @param  mixed  $provider
+	 * @return int
+	 * @throws \RuntimeException
+	 */
 	protected function determineOAuthType($provider)
 	{
 		// Determined based on class name
@@ -339,10 +523,17 @@ class Manager {
 		throw new \RuntimeException('['.get_class($provider).'] does not inherit from a compatible OAuth provider class.');
 	}
 
-	protected function getHighestParent($driver)
+	/**
+	 * Retrieves the highest parent class name for
+	 * the given child class name.
+	 *
+	 * @param  string  $childName
+	 * @return string  $parentName
+	 */
+	protected function getHighestParent($childName)
 	{
 		// Find out what interfaces the driver implements
-		$childClass = new \ReflectionClass($driver);
+		$childClass = new \ReflectionClass($childName);
 
 		while ($parentClass = $childClass->getParentClass())
 		{
@@ -353,6 +544,14 @@ class Manager {
 		return $parentName;
 	}
 
+	/**
+	 * Creates an OAuth1 provider from a connection with an optional
+	 * callback URI.
+	 *
+	 * @param  array  $connection
+	 * @param  string $callbackUri
+	 * @return \League\OAuth1\Client\Server\Server
+	 */
 	protected function createOAuth1Provider($connection, $callbackUri = null)
 	{
 		$driver = $connection['driver'];
@@ -366,6 +565,14 @@ class Manager {
 		return new $driver($credentials);
 	}
 
+	/**
+	 * Creates an OAuth2 provider from a connection with an optional
+	 * callback URI.
+	 *
+	 * @param  array  $connection
+	 * @param  string $callbackUri
+	 * @return \League\OAuth2\Client\Provider\IdentityProvider
+	 */
 	protected function createOAuth2Provider($connection, $callbackUri = null)
 	{
 		$driver = $connection['driver'];
@@ -378,6 +585,23 @@ class Manager {
 		);
 
 		return new $driver($options);
+	}
+
+	/**
+	 * Register an event with Sentry Social.
+	 *
+	 * @param  string   $name
+	 * @param  \Closure $callback
+	 * @return void
+	 */
+	protected function registerEvent($name, Closure $callback)
+	{
+		if ( ! isset($this->dispatcher))
+		{
+			throw new \RuntimeException("Events dispatcher has not been set.");
+		}
+
+		$this->dispatcher->listen("sentry.social.{$name}", $callback);
 	}
 
 	/**
