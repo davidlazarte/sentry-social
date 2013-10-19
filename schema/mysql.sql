89fb61c59f0ce82e849d8b319c226c9e4cf332c4
Christoph Kempen <christoph@downsized.nl>
2013-10-19 20:59:28 +0200

    Fixed another index that was not migrated properly. Also updated the MySQL dump file to the current migration :-)

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -5,18 +5,19 @@ DROP TABLE IF EXISTS `social`;
 
 CREATE TABLE `social` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `service` varchar(127) COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
   `uid` varchar(127) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `access_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `end_of_life` int(11) DEFAULT NULL,
-  `refresh_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `request_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `request_token_secret` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `extra_params` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `access_token_secret` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `provider` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `oauth1_token_identifier` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `oauth1_token_secret` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `oauth2_access_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `oauth2_refresh_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `oauth2_expires` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `social_service_uid_unique` (`service`,`uid`),
-  UNIQUE KEY `social_user_id_service_unique` (`user_id`,`service`),
+  UNIQUE KEY `social_user_id_provider_unique` (`user_id`,`provider`),
+  UNIQUE KEY `social_provider_uid_unique` (`provider`,`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
