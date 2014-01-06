3b2cd33ea2001083c0b9e1c5de409ed67ff53af2
arwho <arwhos@gmail.com>
2014-01-06 16:35:43 -0600

    Update 2013_09_12_161321_migration_cartalyst_sentry_social_alter_links_table.php
    Fixing the down migration to make it compatible with postegres

--- src/migrations/2013_09_12_161321_migration_cartalyst_sentry_social_alter_links_table.php
+++ src/migrations/2013_09_12_161321_migration_cartalyst_sentry_social_alter_links_table.php
@@ -82,9 +82,9 @@ class MigrationCartalystSentrySocialAlterLinksTable extends Migration {
 		Schema::table('social', function($table)
 		{
 			$table->unique(array('user_id', 'service'));
+			$table->dropUnique('social_provider_user_id_unique');
 			$table->dropColumn('provider');
 			$table->string('service');
-			$table->dropUnique('social_provider_user_id_unique');
 			$table->unique(array('service', 'uid'));
 			$table->text('extra_params')->nullable();
 			$table->string('request_token')->nullable();
